3ae3c092d6c8d506df88c933d30d97c4b01abc7d
Vivian Cheng <vivianchengyy@gmail.com>
2023-02-23 01:33:45 +1100

    update unit testing

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
     testEnvironment: 'node',
     testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
+    preset: 'ts-jest',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   };
