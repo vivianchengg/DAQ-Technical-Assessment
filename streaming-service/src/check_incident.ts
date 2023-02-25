56352079b4a5caed5dc01231a500af7296c38a44
Vivian Cheng <vivianchengyy@gmail.com>
2023-02-25 16:42:19 +1100

    update check out of range

--- streaming-service/src/check_incident.ts
+++ streaming-service/src/check_incident.ts
@@ -3,7 +3,7 @@ import fs from 'fs';
 const interval = 5000;
 
 let tempRecord: number[] = [];
-const timeRecord: number[] = [];
+let timeRecord: number[] = [];
 
 const logIncident = (timestamp: number) => {
     const logMsg = `Battery Temperature exceeded Safe Operating Range: ${new Date(timestamp).toISOString()}\n`;
@@ -16,27 +16,25 @@ const logIncident = (timestamp: number) => {
 
 let firstTime = 0;
 export const checkTemp = (temp: number, time: number) => {
-    const now = Date.now();
-
     if (temp < 20 || temp > 80) {
         tempRecord.push(temp);
         timeRecord.push(time);
-    }
 
-    // first instance out of range
-    if (tempRecord.length === 1) {
-        firstTime = time;
+        // first instance out of range
+        if (tempRecord.length === 1) {
+            firstTime = time;
+        }
     }
 
     // when out of range temp appears more than 3 times
     // check whether in 5 seconds
     if (tempRecord.length > 3) {
-        // console.log(now);
-        // console.log(firstTime);
-        if (now - firstTime <= interval) {
+        const now = Date.now();
+        if (time - firstTime <= interval) {
             logIncident(now);
-            // console.log("*****LOGGING TIME*****")
+            // clear past records
             tempRecord = [];
+            timeRecord = [];
         } else {
             tempRecord.shift();
             timeRecord.shift();
